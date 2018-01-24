7040d1b0f7f8ac32eb421414cdf8a3faa0d0a94a
Eden Reich <eden.reich@gmail.com>
2018-01-24 20:44:35 +0100

    modified the name of the test

---
+++ tests/EventManagerTest.js
@@ -0,0 +1,60 @@
+
+import Window from 'window';
+import {assert} from 'chai';
+import Container from '../src/Core/Container.js';
+import EventManager from '../src/Core/EventManager.js';
+
+describe('EventManagerTest', function() {
+
+	beforeEach(function() {	
+		this.container = new Container;
+		this.container.setInstance('Events', new EventManager);
+	});
+
+	afterEach(function() {
+		this.container.flush();
+	});
+
+	it('publish the event to the subscribers', function(done) {
+		let Events = this.container.make('Events');
+		let firstSubscriberWasNotified = false;
+		let secondSubscriberWasNotified = false;
+
+		Events.subscribe('SomeTask', function() {
+			firstSubscriberWasNotified = true;
+		});
+
+		Events.subscribe('SomeTask', function() {
+			secondSubscriberWasNotified = true;
+		});
+
+		setTimeout(function() {
+			// some long operation / tasks.
+			Events.publish('SomeTask');
+			assert.ok(firstSubscriberWasNotified, 'first subscriber was notified');
+			assert.ok(secondSubscriberWasNotified, 'second subscriber was notified');
+			done();
+		}, 5000);
+	}).timeout(10000);
+	
+	it('passes data to the subscribers', function(done) {
+		let Events = this.container.make('Events');
+
+		// first listener.
+		Events.subscribe('SomeTask', function(value) {
+			assert.equal(5, value);
+		});
+
+		// second listener.
+		Events.subscribe('SomeTask', function(value) {
+			assert.equal(5, value);
+			done();
+		});
+
+		setTimeout(function() {
+			// do some tasks.
+			let value = 5;
+			Events.publish('SomeTask', [5]);
+		}, 5000);
+	}).timeout(10000);
+});
